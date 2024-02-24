4e2d123191549d5b5d29f94a3d450dc5984c5691
Jonathan Alberto Guerrero Valdés <92896542+Jonhyz@users.noreply.github.com>
2024-02-24 15:15:18 -0600

    Add files via upload
    Imagenes

---
+++ SweetSapi/imagenes/IS/home.php
@@ -0,0 +1,41 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <div class="Conten">
+        <h1>Leccion 1</h1>
+    </div>
+    <div class="Contenido">
+        <div class="tema">
+            <a href="#">
+				<figure class="img_leccion">
+					<img alt="bloc"  src="bloc.png" class="about_img">
+				</figure>
+			</a>
+            <img alt="bloc"  src="star.png" class="about_img">
+            <h3><br>Introduccion</h3>
+        </div>
+        <div class="tema">
+            <a href="#">
+				<figure class="img_leccion">
+					<img alt="bloc"  src="vacuna.png" class="about_img">
+				</figure>
+			</a>
+            <img alt="bloc"  src="starb.png" class="about_img">
+            <h3><br>Tema 1</h3>
+        </div>
+        <div class="tema">
+            <a href="#">
+				<figure class="img_leccion">
+					<img alt="bloc"  src="frutas.png" class="about_img">
+				</figure>
+			</a>
+            <img alt="bloc"  src="starb.png" class="about_img">
+            <h3><br>Tema 2</h3>
+    </div>
+</body>    
+</html>
